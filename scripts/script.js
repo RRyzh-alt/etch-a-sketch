ec0c824e0fec4199976888b40605759fda66fb99
rryzh <razerchimp12@hotmail.co.uk>
2024-08-17 16:41:46 +0100

    Fix grid creation, scaling and coloring.
    Added new grid creation method, fixed the scaling so that coloring would
    be applied to new grids and not just to the initial grid. Fixed flex
    properties so that the grid always remains the same size and the grid
    items scale to fit the size of the container.

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,57 @@
 //Grid Creation:
 const container = document.querySelector(".container");
 //function to create div with class
-function createDiv() {
-    let div = document.createElement("div");
-    div.className = "grid-div";
-    container.appendChild(div);
 
-    return div;
-}
-let gridRow = 16;
-let gridColumn = 16;
+let gridNum = 16;
 
 function createGrid () {
-    for (i = 0; i<(gridRow * gridColumn); i++) {
-        createDiv();
+    for (let i = 0; i<gridNum; i++) {
+            let div = document.createElement("div");
+            div.className = "grid-row";
+            container.appendChild(div)
+             for  (let x = 0; x<gridNum; x++) {
+                let box = document.createElement("div");
+                box.className = "grid-column"
+                div.appendChild(box)
+             }
     }
 }
 
 createGrid()
+activateColour()
+
+// Change Grid:
+
+let sliderValue = document.querySelector("#range");
+let valueOutput = document.querySelector("#output");
+function removeGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+valueOutput.textContent = sliderValue.value;
+sliderValue.oninput = function () {
+    valueOutput.textContent = this.value; 
+}
+
+sliderValue.addEventListener("change", (event) => {
+    removeGrid();
+     gridNum = sliderValue.value;
+     createGrid()
+     activateColour()
+
+    
+})
 
 //Grid Coloring:
-
-const gridSquares = container.querySelectorAll(".grid-div")
+function activateColour() {
+const gridSquares = container.querySelectorAll(".grid-column")
 
 
 let isColouring = false;
 function toggleColour(event) {
-    if (isColouring) {
+    if (isColouring === true) {
         event.target.style.backgroundColor = "red";
     }  
 }
@@ -52,6 +77,7 @@ const dropOff = container.addEventListener("drop", (event) => {
     event.preventDefault();
 });
 
+
 //Clear Grid:
 const eraser = document.querySelector(".reset");
 
@@ -65,29 +91,7 @@ eraser.addEventListener("click", () => {
     defaultColor()
 });
 
-// Change Grid:
-
-let sliderValue = document.querySelector("#range");
-let valueOutput = document.querySelector("#output");
-function removeGrid() {
-    while (container.firstChild) {
-        container.removeChild(container.lastChild);
-    }
 }
-
-valueOutput.textContent = sliderValue.value;
-sliderValue.oninput = function () {
-    valueOutput.textContent = this.value; 
-}
-
-sliderValue.addEventListener("change", (event) => {
-    removeGrid();
-     gridRow = sliderValue.value;
-     gridColumn = sliderValue.value;
-     createGrid()
-
-    
-})
 
 
 
